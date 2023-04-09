a45c593ebd4a6abf8ca824debbbb737105509de9
Ryoリズキー <riyorizkyprayogi@gmail.com>
2023-04-10 07:54:19 +0800

    Riyo Rizky Prayogi-Pong
    Base Gameplay

---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    public int speed;
+    public KeyCode upKey;
+    public KeyCode downKey;
+
+    private Rigidbody2D rig;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame 
+    void Update()
+    {
+        // gerakan object dengan input
+        MoveObject(GetInput()); 
+    }
+
+    private Vector2 GetInput()
+    {
+        if (Input.GetKey(upKey))
+        {
+           return Vector2.up * speed;
+        }
+        else if (Input.GetKey(downKey))
+        {
+            return Vector2.down * speed;
+        }
+        return Vector2.zero;
+    }
+
+    private void MoveObject(Vector2 movement)
+    {
+        rig.velocity = movement;
+    }
+}
